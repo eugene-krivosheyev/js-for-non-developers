e04fde45920e1cd66476cd0a0d731ec582d38d71
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2020-10-06 12:33:17 +0200

    Modify task codebase.

--- src/js/app.js
+++ src/js/app.js
@@ -22,11 +22,7 @@ function newElement() {
   var inputValue = document.getElementById("myInput").value;
   var t = document.createTextNode(inputValue);
   li.appendChild(t);
-  if (inputValue === '') {
-    alert("You must write something!");
-  } else {
-    document.getElementById("myUL").appendChild(li);
-  }
+  document.getElementById("myUL").appendChild(li);
   document.getElementById("myInput").value = "";
 
   var span = document.createElement("SPAN");
